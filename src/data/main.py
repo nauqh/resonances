be5522b926a672b49879425ae3cb44e2884ddc72
Nauqh <hodominhquan.work@gmail.com>
2023-11-15 14:25:18 +1100

    docs (main.py): add documentation

--- src/data/main.py
+++ src/data/main.py
@@ -2,6 +2,8 @@ from utils import get_token
 from pathlib import Path
 from make_dataset import raw_to_csv, csv_to_combine
 import pandas as pd
+from tqdm import tqdm
+import os
 
 if __name__ == "__main__":
     # TODO: RAW JSON -> CSV SLIDE
@@ -9,11 +11,13 @@ if __name__ == "__main__":
     indir = base / 'raw'
     outdir = base / 'processed'
 
-    raw_to_csv(indir, outdir)
+    # raw_to_csv(indir, outdir)
 
-    # TODO: CSV SLIDE -> ARTISTS, FEATURES
-    slide = pd.read_csv(
-        "D:/Study/Monash/FIT3162/Resonance/data/processed/mpd.slice.0-999.json.csv")
+    # # TODO: CSV SLIDE -> ARTISTS, FEATURES
     token = get_token()
+    fnames = os.listdir(outdir)
 
-    csv_to_combine(slide, token, base)
+    for fname in tqdm(fnames):
+        print(f"Process slide {fname}")
+        slide = pd.read_csv(os.path.join(outdir, fname))
+        csv_to_combine(slide, token, base)
