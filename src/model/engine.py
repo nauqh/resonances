a9211fb5e54b9b5928737c6655da766689d55af2
Nauqh <hodominhquan.work@gmail.com>
2023-12-03 15:37:31 +0700

    refactor (engine.py): rebuild engine using KNN

--- src/model/engine.py
+++ src/model/engine.py
@@ -3,43 +3,48 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 
 
-class Transform():
-    def __init__(self, df: pd.DataFrame):
-        self.df = df
+def select_cols(df: pd.DataFrame, cols_to_select: list):
+    if not set(cols_to_select).issubset(df.columns):
+        raise ValueError("Columns to select do not exist in the DataFrame.")
+    return df[cols_to_select]
 
-    def __select_cols(self, df: pd.DataFrame):
-        return df[['id', 'danceability', 'energy', 'key', 'loudness', 'mode',
-                   'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'popularity']]
 
-    def __ohe(self, df: pd.DataFrame, column: str) -> pd.DataFrame:
-        return pd.get_dummies(df[column], prefix=column, dtype='int').reset_index(drop=True)
+def ohe(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    return pd.get_dummies(df[column], prefix=column, dtype='int').reset_index(drop=True)
 
-    def __create_feature_set(self, df, float_cols) -> pd.DataFrame:
-        scaler = StandardScaler()
 
-        # One-hot Encoding
-        key_ohe = self.__ohe(df, 'key')
-        mode_ohe = self.__ohe(df, 'mode')
+def create_feature_set(df, float_cols) -> pd.DataFrame:
+    scaler = StandardScaler()
 
-        # Scale audio columns
-        floats = df[float_cols].reset_index(drop=True)
-        floats_scaled = pd.DataFrame(
-            scaler.fit_transform(floats), columns=floats.columns)
+    # One-hot Encoding
+    key_ohe = ohe(df, 'key')
+    mode_ohe = ohe(df, 'mode')
 
-        # Concanenate all features
-        final = pd.concat([floats_scaled, key_ohe, mode_ohe], axis=1)
+    # Scale audio columns
+    floats = df[float_cols].reset_index(drop=True)
+    floats_scaled = pd.DataFrame(
+        scaler.fit_transform(floats), columns=floats.columns)
 
-        # Add song id and popularity
-        final.insert(0, "id", df['id'])
-        final['popularity'] = df['popularity']
+    # Concatenate all features
+    final = pd.concat([floats_scaled, key_ohe, mode_ohe], axis=1)
 
-        return final
+    # Add song id and popularity
+    final.insert(0, "id", df['id'])
+    final['popularity'] = df['popularity']
 
-    def process(self):
-        floats = self.df.select_dtypes(include='float64').columns.values
-        self.df = self.__select_cols(self.df)
-        new_df = self.__create_feature_set(self.df, floats)
-        return new_df.sort_values(by='popularity', ascending=False).reset_index(drop=True)
+    return final
+
+
+def process(df):
+    df['mode'] = df['mode'].astype(int)
+    df['key'] = df['key'].astype(int)
+    floats = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+              'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
+
+    cols_to_select = ['id'] + floats + ['popularity']
+    df = select_cols(df, cols_to_select)
+    new_df = create_feature_set(df, floats)
+    return new_df.sort_values(by='popularity', ascending=False).reset_index(drop=True)
 
 
 class KNN():
@@ -53,12 +58,19 @@ class KNN():
         self.neigh.fit(self.df[audio_feats])
 
         n_neighbors = self.neigh.kneighbors(
-            playlist[audio_feats], n_neighbors=5, return_distance=False)[0]
+            playlist[audio_feats], n_neighbors=10, return_distance=False)[0]
         return self.df.iloc[n_neighbors]['id'].tolist()
 
 
-if __name__ == '__main__':
-    tracks = pd.read_csv(
-        "D:/Study/Monash/FIT3162/Resonance/data/combined/tracks/mpd.slice.0-999_tracks.csv")
-    transform = Transform(tracks)
-    df = transform.process()
+if __name__ == "__main__":
+    df = pd.read_csv(
+        "D:/Laboratory/Study/Monash/FIT3162/Resonance/data/Spotify Top Hits/cleaned_track_data.csv")
+    df = df[df['error'].isna()]
+    newdf = process(df)
+
+    playlist = pd.read_csv(
+        "D:/Laboratory/Study/Monash/FIT3162/Resonance/src/data/features.csv")
+    playlist = process(playlist)
+
+    knn = KNN(newdf)
+    recs = knn.recommend(playlist)
