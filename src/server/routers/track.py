f3df8204a62d4de5381d2d8eee5de825c6d8e93f
Nauqh <hodominhquan.work@gmail.com>
2024-01-15 22:47:49 +0700

    refactor (api): cange payload to dict

--- src/server/routers/track.py
+++ src/server/routers/track.py
@@ -11,12 +11,12 @@ router = APIRouter(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_vector(url: str) -> list:
+def create_vector(url: dict) -> list:
     df = pd.read_csv(
         "D:/Laboratory/Study/Monash/FIT3162/Resonance/data/Spotify Top Hits/cleaned_track.csv")
     knn = KNN(df)
     knn.load_model("src/server/engine.pkl")
 
-    playlist = extract_tracks(get_playlist(url))
+    playlist = extract_tracks(get_playlist(url['url']))
 
     return knn.recommend(playlist)
--- tests/test_server.py
+++ tests/test_server.py
@@ -8,7 +8,7 @@ df = pd.read_csv(
 # TODO: QUERY TRACKS
 URL = "http://127.0.0.1:8000/tracks"
 playlist = "https://open.spotify.com/playlist/2xukpbxolEK8C9HdpANzZu?si=7177bd60db6f4271"
-recs = requests.post(URL, params={"url": playlist}).json()
+recs = requests.post(URL, json={"url": playlist}).json()
 print(df[df['id'].isin(recs)].sort_values('popularity'))
 
 # TODO: QUERY ARTISTS
@@ -24,16 +24,3 @@ data = {
 
 with open('./sample.json', 'w') as f:
     json.dump(data, f)
-
-
-def test_extract_songs():
-    URL = "http://127.0.0.1:8000/tracks"
-    playlist = "https://open.spotify.com/playlist/2xukpbxolEK8C9HdpANzZu?si=7177bd60db6f4271"
-    recs = requests.post(URL, params={"url": playlist}).json()
-    print(df[df['id'].isin(recs)].sort_values('popularity'))
-
-
-def text_extract_artists():
-    URL = "http://127.0.0.1:8000/artists"
-    imgs = requests.post(
-        URL, json={"names": ['Justin Bieber', 'Imagine Dragons']}).json()
