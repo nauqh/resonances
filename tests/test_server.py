21e243cf00655ee1959778c2a88de63a77d6dad2
Nauqh <hodominhquan.work@gmail.com>
2024-01-16 15:26:09 +0700

    test (test engine)

--- tests/test_server.py
+++ tests/test_server.py
@@ -16,10 +16,16 @@ URL = "http://127.0.0.1:8000/artists"
 imgs = requests.post(
     URL, json={"names": ['Justin Bieber', 'Imagine Dragons']}).json()
 
+
 # JSON file
 data = {
-    "tracks": df[df['id'].isin(recs)].sort_values('popularity')['id'].tolist(),
-    "artists": imgs
+    "headline": "uplifting - harmonious - lyrical",
+    "characteristics": [
+        "Your music taste leans a bit towards Indie Folk, characterized by its melodic simplicity and heartfelt lyrics. Typical representation of this taste encompass acoustic instruments like guitars, banjos, and harmonicas, often accompanied by soft, emotive vocals.",
+        "This genre tends to strike a balance between melancholic and uplifting tones, offering a mix of introspective, soul-searching ballads and cheerful, foot-tapping tunes. The tempo is generally moderate, allowing for a comfortable sway between introspection and celebration. Indie Folk resonates with a raw, organic quality, inviting listeners to connect with its unfiltered emotions."
+    ],
+    "artists": imgs,
+    "tracks": df[df['id'].isin(recs)].sort_values('popularity')['id'].tolist()
 }
 
 with open('./sample.json', 'w') as f:
