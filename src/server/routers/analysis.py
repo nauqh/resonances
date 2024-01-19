7a2fc2b433654897b47c57f12c8308a37a3b250c
Nauqh <hodominhquan.work@gmail.com>
2024-01-19 15:18:28 +0700

    integrate (llm): integrate llm endpoint

--- src/server/routers/analysis.py
+++ src/server/routers/analysis.py
@@ -1,4 +1,6 @@
 from fastapi import status, APIRouter
+from ...utils.config import settings
+from ..llm import LLM
 
 router = APIRouter(
     prefix="/analysis",
@@ -7,5 +9,6 @@ router = APIRouter(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def generate_analysis(genre: dict):
-    return genre
+def generate_analysis(description: dict):
+    data = LLM(settings.OPENAI_KEY).analyze(description)
+    return data
