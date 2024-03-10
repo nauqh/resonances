d2ef6088ff7275e88ecddfe780f153f48c62277d
rumi <hilmy.mul@gmail.com>
2024-03-10 23:32:50 +1100

    example json api call

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import json
 # run python3 -m backend.app.main once pydantic is setup properly
-from ..src.utils import utils
+#from ..src.utils import utils
 
 app = FastAPI(title='Resonance', version='1.0.0')
 
@@ -16,13 +17,19 @@ app.add_middleware(
 def root():
     return {"message": "Root endpoint"}
 
+@app.get("/example/{example_name}")     # example json api call, will find the json file (currently in the same dir, may change later)
+def example(example_name):
+    with open(f'{example_name}.json') as ex:
+        ex_j = json.load(ex)
+    ex.close()
+    return ex_j
+
 @app.get("/generate/{spotify_account}")
-def generate_taste():
+def generate_taste(spotify_account):
     #todo call individual data jsons, pass spotify account into the ones that need them
-    return {"test"}
+    return {"test", spotify_account}
 
 # build individual data jsons, use utils for playlists and artists from spotify api
-
 def get_artists():
     return {"artists"}
 
